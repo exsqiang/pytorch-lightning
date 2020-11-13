b6dcc07968ee7864a7ba27c5b29cdcf95bc0136e
SeanNaren <sean@grid.ai>
2020-11-13 11:11:42 +0000

    debug

--- pytorch_lightning/plugins/native_amp.py
+++ pytorch_lightning/plugins/native_amp.py
@@ -79,9 +79,11 @@ class LightningGradScaler(torch.cuda.amp.GradScaler):
                 self.unscale_(optimizer)
 
         assert len(optimizer_state["found_inf_per_device"]) > 0, "No inf checks were recorded for this optimizer."
+        with self.trainer.profiler.profile("amp scaler optim get inf sum"):
+            items = (v.item() for v in optimizer_state["found_inf_per_device"].values())
         with self.trainer.profiler.profile("amp scaler optim step inf sum"):
-            num_infs = sum(v.item() for v in optimizer_state["found_inf_per_device"].values())
-        print("INF DEVICE", len(optimizer_state["found_inf_per_device"].values()))
+            num_infs = sum(items)
+        print("NUM INF DEVICE", len(optimizer_state["found_inf_per_device"].values()))
         with self.trainer.profiler.profile("amp scaler optim step inf loop"):
             if not num_infs:
                 with self.trainer.profiler.profile("amp scaler optim step"):
