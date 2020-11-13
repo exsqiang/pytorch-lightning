7e37b00f88cdc4aa212d730448d82bb78f09c897
SeanNaren <sean@grid.ai>
2020-11-13 10:14:40 +0000

    debug

--- pytorch_lightning/plugins/native_amp.py
+++ pytorch_lightning/plugins/native_amp.py
@@ -14,9 +14,80 @@
 from typing import Union
 
 import torch
+from torch.cuda.amp.grad_scaler import OptState
 from torch.optim import Optimizer
 
 from pytorch_lightning.plugins.precision_plugin import PrecisionPlugin
+
+
+class LightningGradScaler(torch.cuda.amp.GradScaler):
+
+    def __init__(self, trainer, init_scale=2. ** 16, growth_factor=2.0, backoff_factor=0.5, growth_interval=2000,
+                 enabled=True):
+
+        self.trainer = trainer
+
+        super().__init__(init_scale, growth_factor, backoff_factor, growth_interval, enabled)
+
+    def step(self, optimizer, *args, **kwargs):
+        """
+        :meth:`step` carries out the following two operations:
+
+        1.  Internally invokes ``unscale_(optimizer)`` (unless :meth:`unscale_` was explicitly called for ``optimizer``
+            earlier in the iteration).  As part of the :meth:`unscale_`, gradients are checked for infs/NaNs.
+        2.  If no inf/NaN gradients are found, invokes ``optimizer.step()`` using the unscaled
+            gradients.  Otherwise, ``optimizer.step()`` is skipped to avoid corrupting the params.
+
+        ``*args`` and ``**kwargs`` are forwarded to ``optimizer.step()``.
+
+        Returns the return value of ``optimizer.step(*args, **kwargs)``.
+
+        Arguments:
+            optimizer (torch.optim.Optimizer):  Optimizer that applies the gradients.
+            args:  Any arguments.
+            kwargs:  Any keyword arguments.
+
+        .. warning::
+            Closure use is not currently supported.
+        """
+        if (not self._enabled):
+            return optimizer.step(*args, **kwargs)
+
+        if "closure" in kwargs:
+            raise RuntimeError("Closure use is not currently supported if GradScaler is enabled.")
+        with self.trainer.profiler.profile("amp scaler check scale growth"):
+            self._check_scale_growth_tracker("step")
+
+        optimizer_state = self._per_optimizer_states[id(optimizer)]
+
+        if optimizer_state["stage"] is OptState.STEPPED:
+            raise RuntimeError("step() has already been called since the last update().")
+
+        retval = None
+
+        if (hasattr(optimizer, "_step_supports_amp_scaling") and optimizer._step_supports_amp_scaling):
+            # This optimizer has customized scale-handling logic, so we can call optimizer.step() directly.
+            # The contract with custom optimizers is that their step() should accept an additional,
+            # optional grad_scaler kwarg.  We append self to the kwargs so the custom optimizer has full information:
+            # it can query its own state, invoke unscale_ on itself, etc
+            retval = optimizer.step(*args, **dict(kwargs, grad_scaler=self))
+            optimizer_state["stage"] = OptState.STEPPED
+            return retval
+
+        with self.trainer.profiler.profile("amp scaler unscale"):
+            if optimizer_state["stage"] is OptState.READY:
+                self.unscale_(optimizer)
+
+        assert len(optimizer_state["found_inf_per_device"]) > 0, "No inf checks were recorded for this optimizer."
+
+        with self.trainer.profiler.profile("amp scaler optim step inf loop"):
+            if not sum(v.item() for v in optimizer_state["found_inf_per_device"].values()):
+                with self.trainer.profiler.profile("amp scaler optim step"):
+                    retval = optimizer.step(*args, **kwargs)
+
+        optimizer_state["stage"] = OptState.STEPPED
+
+        return retval
 
 
 class NativeAMPPlugin(PrecisionPlugin):
@@ -59,7 +130,7 @@ class NativeAMPPlugin(PrecisionPlugin):
 
     @property
     def scaler(self):
-        return torch.cuda.amp.GradScaler()
+        return LightningGradScaler(self.trainer)
 
     def clip_gradients(self, grad_clip_val: Union[int, float], optimizer: Optimizer, norm_type: float):
         model = self.trainer.get_model()
